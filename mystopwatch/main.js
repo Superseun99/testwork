5399d7189b51bacc77c9f4e752215315007f8976
superseun <67711095+Superseun99@users.noreply.github.com>
2020-11-17 12:32:04 +0100

    A runner stopwatch
    A simple stopwatch that could be used by athlete to time their activities.

--- mystopwatch/main.js
+++ mystopwatch/main.js
@@ -1,39 +1,70 @@
-test=0;
-a=0;
-function aut(){
-    if(test<1){
-        a=setInterval(function() {
-            add();
-        },10);
-        test++;
+
+    var interval;
+    var seconds = 00;
+    var tens = 00;
+    var minutes = 00;
+    var start = document.getElementById("button-start");
+    var stop = document.getElementById("button-stop");
+    var reset = document.getElementById("button-reset");
+    var showMinutes = document.getElementById("minutes");
+    var showSeconds = document.getElementById("seconds");
+    var showTens = document.getElementById("tens");
+    var colonImp = document.getElementById("colon");
+
+    start.onclick = function () {
+        clearInterval(interval);
+        interval = setInterval(startTimer, 10);
+        stop.style.display = "inline-block";
+        reset.style.display = "inline-block";
+        start.style.display = "none";
     }
-}
-ms = 0;
-s = 0;
-min =0;
-function add() {
-    ++ms;
-    if (ms>=100) {ms-=100; ++s;}
-    if (ms<10) ms = "0"+ms;
-    if (s<10&&ms==0||s==0&&ms==1) s = "0"+s;
-    if (s>=60) {s-=60;s=String(s).slice(0,1);  ++min}
-    if (min<10&&s<1&&ms==1) min = "0"+min;
-    $(".end").text(min+" : "+s+" : "+ms);
-}
-function stop_aut() {
-    clearInterval(a);
-    test=0;
-}
-function reset(){
-    clearInterval(a);
-    ms=0;
-    s=0; 
-    min=0;
-    $(".end").text("0"+min+" : 0"+s+" : 0"+ms);
-    test=0;
-    $("#lapEnd").children().remove();
-}
-function lap(){
-    lapVal=min+":"+s+":"+ms;
-    $("#lapEnd").append($("<li></li>").text(lapVal));
-}
+
+    stop.onclick = function () {
+        clearInterval(interval);
+        start.style.display = "inline-block";
+        stop.style.display = "none";
+    }
+
+    reset.onclick = function () {
+        clearInterval(interval);
+        seconds = 00;
+        tens = 00;
+        minutes = 0;
+        showMinutes.innerHTML = "0" + minutes;
+        showSeconds.innerHTML = "0" + seconds;
+        showTens.innerHTML = "0" + tens;
+        start.style.display = "inline-block";
+        stop.style.display = "none";
+        reset.style.display = "none";
+    }
+
+    function startTimer() {
+        tens++;
+        if (tens <= 9) {
+            showTens.innerHTML = "0" + tens;
+        }
+        if (tens > 9) {
+            showTens.innerHTML = tens;
+        }
+        if (tens > 99) {
+            seconds++;
+            tens = 0;
+            showTens.innerHTML = "0" + 0;
+            showSeconds.innerHTML = seconds;
+        }
+        if (seconds <= 9) {
+            showSeconds.innerHTML = "0" + seconds;
+        }
+        if (seconds > 9) {
+            showSeconds.innerHTML = seconds;
+        }
+        if (seconds % 60 == 0 && seconds > 0) {
+            seconds = 00;
+            showSeconds.innerHTML = seconds;
+            minutes++;
+            colonImp.style.display = "inline-block";
+            showMinutes.style.display = "inline-block";
+            showMinutes.innerHTML = " " + minutes;
+        }
+
+    }
